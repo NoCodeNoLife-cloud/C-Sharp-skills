01dc970f3a40cc065000e7362958bae48837b693
NoCodeNolife-cloud <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2022-07-29 16:30:01 +0800

    update

---
+++ Collections/Hashtable/Hashtable/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+
+namespace HashtableDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // assignment
+            Hashtable hashtable = new Hashtable();
+
+            // GetHash(Object)
+            Console.Out.WriteLine($"hashtable.GetHashCode()={hashtable.GetHashCode()}");
+
+            // GetType()
+            Console.Out.WriteLine($"hashtable.GetType()={hashtable.GetType()}");
+
+            // ToString()
+            Console.Out.WriteLine($"hashtable.ToString()={hashtable.ToString()}");
+
+            // IsFixedSize
+            Console.Out.WriteLine($"hashtable.IsFixedSize={hashtable.IsFixedSize}");
+
+            // IsReadOnly
+            Console.Out.WriteLine($"hashtable.IsReadOnly={hashtable.IsReadOnly}");
+
+            // IsSynchronized
+            Console.Out.WriteLine($"hashtable.IsSynchronized={hashtable.IsSynchronized}");
+
+            // Add(Object, Object)
+            hashtable.Add(5, 4);
+            hashtable.Add("hello", "world");
+
+            // Count
+            Console.Out.WriteLine($"hashtable.Count={hashtable.Count}");
+
+            // GetEnumerator()
+            Console.Out.WriteLine("GetEnumerator()");
+            IDictionaryEnumerator dictionaryEnumerator = hashtable.GetEnumerator();
+            while (dictionaryEnumerator.MoveNext())
+            {
+                Console.Out.Write($"{dictionaryEnumerator.Current} ");
+            }
+
+            Console.Out.WriteLine();
+
+            // Remove(Object)
+            Console.WriteLine("Remove(Object)");
+            hashtable.Remove("hello");
+
+            // Contains(Object)
+            Console.Out.WriteLine($"hashtable.Contains(\"hello\")={hashtable.Contains("hello")}");
+
+            // ContainsKey(Object)
+            Console.Out.WriteLine($"hashtable.ContainsKey(\"8\")={hashtable.ContainsKey("8")}");
+
+            // ContainsValue(Object)
+            Console.Out.WriteLine($"hashtable.ContainsValue(\"4\")={hashtable.ContainsValue("4")}");
+            // Item[Object]
+            Console.Out.WriteLine($"hashtable[5]={hashtable[5]}");
+
+            // Keys
+            Console.Out.WriteLine("Keys");
+            IEnumerator enumerator = hashtable.Keys.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.Out.Write($"{enumerator.Current} ");
+            }
+
+            Console.Out.WriteLine();
+
+            // Values
+            Console.Out.WriteLine("Values");
+            enumerator = hashtable.Values.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.Out.Write($"{enumerator.Current} ");
+            }
+
+            Console.Out.WriteLine();
+
+            // Clone()
+            Hashtable cHashtable = (Hashtable) hashtable.Clone();
+
+            // Equals(Object)
+            Console.Out.WriteLine($"hashtable.Equals(cHashtable)={hashtable.Equals(cHashtable)}");
+
+            // Clear()
+            hashtable.Clear();
+        }
+    }
+}
---
+++ Collections/SortedSet/SortedSet/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedSetDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // assignment
+            SortedSet<int> sortedSet = new SortedSet<int>();
+
+            // GetHashCode()
+            Console.Out.WriteLine($"sortedSet.GetHashCode()={sortedSet.GetHashCode()}");
+
+            // GetType()
+            Console.Out.WriteLine($"sortedSet.GetType()={sortedSet.GetType()}");
+
+            // ToString()
+            Console.Out.WriteLine($"sortedSet.ToString()={sortedSet.ToString()}");
+
+            // Add(T)
+            Console.Out.WriteLine("Add(T)");
+            sortedSet.Add(4);
+            sortedSet.Add(7);
+            sortedSet.Add(9);
+            sortedSet.Add(5);
+            sortedSet.Add(3);
+
+            // GetEnumerator()
+            Console.Out.WriteLine("GetEnumerator()");
+            SortedSet<int>.Enumerator enumerator = sortedSet.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.Out.Write($"{enumerator.Current} ");
+            }
+
+            Console.Out.WriteLine();
+
+            // Reverse()
+            Console.Out.WriteLine("Reverse()");
+            IEnumerator<int> iEnumerator = sortedSet.Reverse().GetEnumerator();
+            while (iEnumerator.MoveNext())
+            {
+                Console.Out.Write($"{iEnumerator.Current} ");
+            }
+
+            Console.Out.WriteLine();
+
+            // GetViewBetween(T,  T)
+            Console.Out.WriteLine("GetViewBetween(T,  T)");
+            enumerator = sortedSet.GetViewBetween(6, 10).GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.Out.Write($"{enumerator.Current} ");
+            }
+
+            Console.Out.WriteLine();
+
+            // Equals(Object)
+            Console.Out.WriteLine($"sortedSet.Equals(sortedSet)={sortedSet.Equals(sortedSet)}");
+
+            // Contains(T)
+            Console.Out.WriteLine($"sortedSet.Contains(5)={sortedSet.Contains(5)}");
+
+            List<int> list = new List<int>();
+            list.Add(3);
+            list.Add(5);
+            list.Add(7);
+
+            // SetEquals(IEnumerable<T>)
+            Console.Out.WriteLine($"sortedSet.SetEquals(list)={sortedSet.SetEquals(list)}");
+
+            // IsProperSubsetOf(IEnumerable<T>)
+            Console.Out.WriteLine($"sortedSet.IsProperSubsetOf(list)={sortedSet.IsProperSubsetOf(list)}");
+
+            // IsProperSupersetOf(IEnumerable<T>)
+            Console.Out.WriteLine($"sortedSet.IsProperSubsetOf(list)={sortedSet.IsProperSubsetOf(list)}");
+
+            // IsSubsetOf(IEnumerable<T>)
+            Console.Out.WriteLine($"sortedSet.IsSubsetOf(list)={sortedSet.IsSubsetOf(list)}");
+
+            // IsSupersetOf(IEnumerable<T>)
+            Console.Out.WriteLine($"sortedSet.IsSupersetOf(list)={sortedSet.IsSupersetOf(list)}");
+
+            // ExceptWith(IEnumerable<T>)
+            sortedSet.ExceptWith(list);
+
+            // CopyTo(T[])
+            Console.Out.WriteLine("CopyTo(T[])");
+            int[] ints = new int[sortedSet.Count];
+            sortedSet.CopyTo(ints);
+            foreach (int item in ints)
+            {
+                Console.Out.Write($"{item} ");
+            }
+
+            Console.Out.WriteLine();
+
+            // Count
+            Console.Out.WriteLine($"sortedSet.Count={sortedSet.Count}");
+
+            // Max
+            Console.Out.WriteLine($"sortedSet.Max={sortedSet.Max}");
+
+            // Remove(T)
+            Console.Out.WriteLine("Remove(T)");
+            sortedSet.Remove(sortedSet.Max);
+
+            // Min
+            Console.Out.WriteLine($"sortedSet.Min={sortedSet.Min}");
+
+            // RemoveWhere(Predicate<T>)
+            Console.Out.WriteLine("RemoveWhere(Predicate<T>)");
+            sortedSet.RemoveWhere(RemoveWherePredicate);
+
+            // Clear()
+            sortedSet.Clear();
+        }
+
+        static bool RemoveWherePredicate(int i)
+        {
+            return (i % 2 == 0) ? true : false;
+        }
+    }
+}
