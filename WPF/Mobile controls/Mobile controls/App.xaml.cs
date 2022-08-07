a8f6e45c3606442946e2d341197d04f878eddc44
NoCodeNolife-cloud <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2022-08-07 09:56:23 +0800

    update

---
+++ WPF/Mobile controls/Mobile controls/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Mobile_controls
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ WPF/Mobile controls/Mobile controls/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Mobile_controls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private Point _mouseLocate;
+        private Point _controlLocate;
+        private bool _mouseDown;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _mouseDown = false;
+        }
+
+        private void Button_OnMousePreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _mouseDown = true;
+            Control control = sender as Control;
+            _mouseLocate = e.GetPosition(this);
+            TranslateTransform transform = control.RenderTransform as TranslateTransform;
+            if (transform == null)
+            {
+                transform = new TranslateTransform();
+                control.RenderTransform = transform;
+            }
+
+            _controlLocate.X = transform.X;
+            _controlLocate.Y = transform.Y;
+        }
+
+        private void Button_OnMousePreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (_mouseDown)
+            {
+                Control control = sender as Control;
+                Point nowLocate = e.GetPosition(this);
+                Vector vector = nowLocate - _mouseLocate;
+                TranslateTransform transform = control.RenderTransform as TranslateTransform;
+                transform.X = _controlLocate.X + vector.X;
+                transform.Y = _controlLocate.Y + vector.Y;
+            }
+        }
+
+        private void Button_OnMousePreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            _mouseDown = false;
+        }
+    }
+}
