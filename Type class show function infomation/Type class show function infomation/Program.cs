8d7670fb67374ddc5138777e04b534c5c89b8b91
NoCodeNolife-cloud <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2022-09-26 10:59:24 +0800

    update

---
+++ Type class show function infomation/Type class show function infomation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace Type_class_show_function_infomation
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 4;
+            Type intType = i.GetType();
+            MethodInfo[] methodInfos = intType.GetMethods();
+            foreach (MethodInfo methodInfo in methodInfos)
+            {
+                Console.Out.WriteLine($"methodInfo.MemberType={methodInfo.MemberType}");
+                Console.Out.WriteLine($"methodInfo.ReturnParameter={methodInfo.ReturnParameter}");
+                Console.Out.WriteLine($"methodInfo.ReturnType={methodInfo.ReturnType}");
+                Console.Out.WriteLine($"methodInfo.ReturnTypeCustomAttributes={methodInfo.ReturnTypeCustomAttributes}");
+                Console.Out.WriteLine($"methodInfo.Attributes={methodInfo.Attributes}");
+            }
+        }
+    }
+}
